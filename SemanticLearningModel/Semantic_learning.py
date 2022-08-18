9c3d6ad4abf63e08bfe767c6eee4e8f9002af788
JirkoRu <jirko.rubruck@psy.ox.ac.uk>
2022-08-18 15:38:06 +0100

    added a plotting function keeping track of inputs and outputs

--- SemanticLearningModel/Semantic_learning.py
+++ SemanticLearningModel/Semantic_learning.py
@@ -1,3 +1,4 @@
+from importlib import import_module
 from matplotlib import pyplot as plt
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
@@ -5,10 +6,13 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import os
+
+dname = os.getcwd() + "/PycharmProjects/SemanticLearningModel/SemanticLearningModel/"
 
 # task specific parameters
 n_examples = 4     # n_examples should be divisible by n_classes
-n_features = 6
+n_features = 7
 n_classes = 4
 
 # hyperparameters
@@ -20,8 +24,8 @@ batch_size = 4
 learning_rate = 1/n_examples
 
 # a dictionary of indices of present features in each class
-class_index_dict = {"a1_b1": (0, 2), "a1_b2": (0, 3),
-                    "a2_c1": (1, 4), "a2_c2": (1, 5)
+class_index_dict = {"a1_b1": (0, 1, 3), "a1_b2": (0, 1, 4),
+                    "a2_c1": (0, 2, 5), "a2_c2": (0, 2, 6)
                     }
 
 
@@ -52,6 +56,7 @@ class DatasetGenerator():
 
             self.features[value[0], lower_col_idx: higher_col_idx] = 1 * (0.7) *4
             self.features[value[1], lower_col_idx: higher_col_idx] = 1 * (0.7) *4
+            self.features[value[2], lower_col_idx: higher_col_idx] = 1 * (0.7) *4
             self.labels[lower_col_idx: higher_col_idx] = count
 
         self.labels = F.one_hot(self.labels.to(torch.int64), num_classes=self.n_classes)
@@ -79,11 +84,16 @@ class CustomDataset(Dataset):
 class FullyConnected(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(FullyConnected, self).__init__()
+
         self.fully_con1 = nn.Linear(input_size, hidden_size)
         torch.nn.init.normal_(self.fully_con1.weight, mean=0, std=0.0001/input_size)
+        torch.nn.init.normal_(self.fully_con1.bias, mean=0, std=0.0001/input_size)
+
         # self.relu = nn.ReLU()
+
         self.fully_con2 = nn.Linear(hidden_size, output_size)
         torch.nn.init.normal_(self.fully_con2.weight, mean=0, std=0.0001/output_size)
+        torch.nn.init.normal_(self.fully_con2.bias, mean=0, std=0.0001/output_size)
 
     def forward(self, x):
         x = self.fully_con1(x)
@@ -99,12 +109,26 @@ if __name__ == "__main__":
     we generate first a general and then a pytorch specific dataset, to feed it to our dataloader
     to change prediction from features to class just swap the two terms: labels, features
     """
+    # train data
     data_generator = DatasetGenerator(n_examples, n_features, n_classes, class_index_dict)
     features, labels = data_generator.generate_dataset()
     train_set = CustomDataset(input_tensors=(labels, features))
     train_loader = DataLoader(train_set,
                               batch_size=batch_size,
                               shuffle=True)
+    # test data
+    test_data_generator = DatasetGenerator(n_examples,
+                                           n_features,
+                                           n_classes,
+                                           class_index_dict
+                                           )
+
+    test_features, test_labels = test_data_generator.generate_dataset()
+    test_set = CustomDataset(input_tensors=(test_labels, test_features))
+    test_loader = DataLoader(test_set,
+                             batch_size=batch_size,
+                             shuffle=False
+                             )
 
     """
     define the network, loss-function, and optimiser
@@ -116,34 +140,74 @@ if __name__ == "__main__":
     optimiser = optim.SGD(network.parameters(), lr=learning_rate)
 
     # lets train
-    loss_history = []
+    # make a history of losses
+    loss_history_train = []
+
+    # save the inputs and outputs in a list
+    inputs  = np.full([4, 4, n_epochs], np.nan)
+    outputs = np.full([4, 7, n_epochs], np.nan)
+
+
     for epoch in range(n_epochs):
         correct = 0
+        running_loss_train = 0
+
         for i, data in enumerate(train_loader):
-            x, y = data
+            x_train, y_train = data
 
             # first forward pass
-            hidden_act, out = network(x)
+            hidden_act, out = network(x_train)
 
-            loss = loss_func(out, y)
+            # compute loss
+            loss = loss_func(out, y_train)
 
             # backpropagation
             optimiser.zero_grad()
             loss.backward()
             optimiser.step()
 
-            # use y and x as numpy arrays for taking track of correct categorisations
-            out_rounded = np.round(out.cpu().detach().numpy(), decimals=1)
-            y_rouned = np.round(y.cpu().detach().numpy(), decimals=1)
+            # keep track of loss
+            running_loss_train += loss.item()
+
+            # use y and x as numpy arrays for keeping track of correct categorisations
+            out_rounded = np.round(out.cpu().detach().numpy(), decimals=2)
+            y_rouned = np.round(y_train.cpu().detach().numpy(), decimals=2)
             correct += np.sum(np.all(out_rounded == y_rouned, axis=1))
 
-            if (i + 1) % (len(train_set)/batch_size) == 0:
-                loss_history.append(loss.item())
-                print("Epoch[{}/{}], Loss: {:.4f}, Accuracy: {} %".format(epoch+1, n_epochs,
-                                                                 loss.item(),
-                                                                 (100 * correct / len(train_set))))
-    plt.plot(np.linspace(0, n_epochs, n_epochs), loss_history)
+            # store the outputs and inputs
+            inputs[:, :, epoch] = x_train.detach().numpy()
+            outputs[:,:, epoch] = out.detach().numpy()
+
+        with torch.no_grad():
+            for i, data in enumerate(test_loader):
+                x_test, y_test = data
+
+                # first forward pass
+                hidden_act, out_test = network(x_test)
+                loss_test = loss_func(out_test, y_test)
+
+        # calculate train and test loss for epoch, append, and print
+
+        epoch_train_loss = running_loss_train / len(train_loader)
+        loss_history_train.append(epoch_train_loss)
+
+        print("Epoch[{}/{}], Train Loss: {:.4f}, Accuracy: {} %".format(epoch + 1, n_epochs,
+                                                                  epoch_train_loss,
+                                                                  (100 * correct / len(train_set))))
+
+    # save the input output matrices
+
+    
+    # save the model weights
+    torch.save(network.state_dict(), dname + "saved_weights/relu_small_weights_16hidden.pt")
+
+    # little plotty plot
+    plt.plot(np.linspace(0, n_epochs, n_epochs), loss_history_train, label="train loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Mean squared error")
+    plt.legend()
     plt.show()
+    plt.savefig(dname + "figures/loss_semantic_learning_linear_16hidden.png")
 
 
 
--- SemanticLearningModel/Semantic_learning_relu.py
+++ SemanticLearningModel/Semantic_learning_relu.py
@@ -1,3 +1,4 @@
+from audioop import bias
 from matplotlib import pyplot as plt
 import numpy as np
 from torch.utils.data import Dataset, DataLoader
@@ -6,10 +7,12 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from Semantic_learning import DatasetGenerator, CustomDataset
+import os
 
+dname = os.getcwd() + "/PycharmProjects/SemanticLearningModel/SemanticLearningModel/"
 # task specific parameters
 n_examples = 4     # n_examples should be divisible by n_classes
-n_features = 6
+n_features = 7
 n_classes = 4
 
 # hyperparameters
@@ -21,20 +24,24 @@ batch_size = 4
 learning_rate = 1/n_examples
 
 # a dictionary of indices of present features in each class
-class_index_dict = {"a1_b1": (0, 2), "a1_b2": (0, 3),
-                    "a2_c1": (1, 4), "a2_c2": (1, 5)
+class_index_dict = {"a1_b1": (0, 1, 3), "a1_b2": (0, 1, 4),
+                    "a2_c1": (0, 2, 5), "a2_c2": (0, 2, 6)
                     }
-
                 
 # define our model class
 class FullyConnected(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
         super(FullyConnected, self).__init__()
+
         self.fully_con1 = nn.Linear(input_size, hidden_size)
         torch.nn.init.normal_(self.fully_con1.weight, mean=0, std=0.0001/input_size)
+        torch.nn.init.normal_(self.fully_con1.bias, mean=0, std=0.0001/input_size)
+
         self.relu = nn.ReLU()
+
         self.fully_con2 = nn.Linear(hidden_size, output_size)
         torch.nn.init.normal_(self.fully_con2.weight, mean=0, std=0.0001/output_size)
+        torch.nn.init.normal_(self.fully_con2.bias, mean=0, std=0.0001/output_size)
 
     def forward(self, x):
         x = self.fully_con1(x)
@@ -77,7 +84,7 @@ if __name__ == "__main__":
     test_set = CustomDataset(input_tensors=(test_labels, test_features))
     test_loader = DataLoader(test_set,
                              batch_size=batch_size,
-                             shuffle=True
+                             shuffle=False
                              )
 
     """
@@ -99,7 +106,6 @@ if __name__ == "__main__":
     for epoch in range(n_epochs):
         correct = 0
         running_loss_train = 0
-        running_loss_test = 0
 
         for i, data in enumerate(train_loader):
             x_train, y_train = data
@@ -118,7 +124,7 @@ if __name__ == "__main__":
             # keep track of loss
             running_loss_train += loss.item()
 
-            # use y and x as numpy arrays for taking track of correct categorisations
+            # use y and x as numpy arrays for keeping track of correct categorisations
             out_rounded = np.round(out.cpu().detach().numpy(), decimals=2)
             y_rouned = np.round(y_train.cpu().detach().numpy(), decimals=2)
             correct += np.sum(np.all(out_rounded == y_rouned, axis=1))
@@ -130,28 +136,23 @@ if __name__ == "__main__":
                 # first forward pass
                 hidden_act, out_test = network(x_test)
                 loss_test = loss_func(out_test, y_test)
-                running_loss_test += loss_test.item()
 
         # calculate train and test loss for epoch, append, and print
 
         epoch_train_loss = running_loss_train / len(train_loader)
         loss_history_train.append(epoch_train_loss)
 
-        epoch_test_loss = running_loss_test / len(test_loader)
-        loss_history_test.append(epoch_test_loss)
-
         print("Epoch[{}/{}], Train Loss: {:.4f}, Accuracy: {} %".format(epoch + 1, n_epochs,
                                                                   epoch_train_loss,
                                                                   (100 * correct / len(train_set))))
-        print("Epoch[{}/{}],Test Loss: {:.4f}".format(epoch+1, n_epochs, epoch_test_loss))
 
     # save the model weights
-    torch.save(network.state_dict(), "linear_small_weights_100hidden.pt")
+    torch.save(network.state_dict(), dname + "saved_weights/relu_small_weights_16hidden.pt")
 
+    # little plotty plot
     plt.plot(np.linspace(0, n_epochs, n_epochs), loss_history_train, label="train loss")
-    plt.plot(np.linspace(0, n_epochs, n_epochs), loss_history_test, label="test loss")
     plt.xlabel("Epochs")
     plt.ylabel("Mean squared error")
     plt.legend()
     plt.show()
-    plt.savefig("figures/loss_semantic_learning_relu_16hidden.png")
+    plt.savefig(dname + "figures/loss_semantic_learning_relu_16hidden.png")
